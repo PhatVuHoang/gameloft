cf3551901ca189d3771f7ae7e3f6fec58977ecf3
PhatVH <phatvh.work@gmail.com>
2023-11-14 20:52:25 +0700

    fix: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import HamburgerIcon from '@/assets/hamburger-icon.svg'
 
 export default function Header() {
   return (
-    <header className='bg-white opacity-60 flex justify-between items-center lg:px-20 px-3 fixed md:w-[calc(100%-5px)] w-full'>
+    <header className='bg-white opacity-60 flex justify-between items-center lg:px-20 px-3 fixed md:w-[calc(100%-5px)] w-full z-[99]'>
       <img src={Logo} alt='logo' width={229} />
       <img src={HamburgerIcon} alt='icon' width={40} />
     </header>
